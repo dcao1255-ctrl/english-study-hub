e8eb9bcdf88115b030a12608db4eba59e61650da
Clara0809 <dcao1255@gmail.com>
2026-07-30 16:15:16 +0800

    Delete assets directory

--- assets/js/supabase-config.js
+++
@@ -1,7 +0,0 @@
-// Supabase 的 URL 与 anon key 可以公开；service_role key 绝不能放入前端或 GitHub。
-window.STUDY_HUB_CONFIG = {
-  SUPABASE_URL: "https://zxqwsteefbuxkjazzfah.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_fR99bbda-i8c2hfFIBSD2A_JrhnhoMV",
-  USERNAME_DOMAIN: "qq.com",
-  MATERIALS_BUCKET: "student-materials"
-};
