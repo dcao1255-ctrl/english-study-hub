47c10a5d548ff573ae00878c9d9ee935813cf685
Clara0809 <dcao1255@gmail.com>
2026-07-30 15:03:30 +0800

    Update supabase-config.js

--- docs/assets/js/supabase-config.js
+++ docs/assets/js/supabase-config.js
@@ -1,5 +1,5 @@
 window.STUDY_HUB_CONFIG = {
   SUPABASE_URL: "https://zxqwsteefbuxkjazzfah.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_fR99bbda-i8c2hfFIBSD2A_JrhnhoMV",
-  USERNAME_DOMAIN: "students.example.com"
+  USERNAME_DOMAIN: "qq.com"
 };
